f91442381269d98dcc528d2a9e8f90604137f041
Ramiro <msr.ramiro@gmail.com>
2019-07-06 01:49:21 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port=process.env.PORT || 3000;
+app.use(express.static(__dirname+'/public'));
+ //Express HB engine
+ hbs.registerPartials(__dirname+'/views/parciales/')
+app.set('view engine','hbs');
+
+
+app.get('/', function (req, res) {   
+    res.render('home',{
+        nombre:'ramiro',
+       
+    });
+});
+app.get('/about',function(req,res) {
+    res.render('about',{
+        
+    });
+})
+app.get('/data',(req,res)=>{
+res.send('Hola data');
+}) 
+
+app.listen(port,()=>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
